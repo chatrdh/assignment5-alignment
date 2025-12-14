c54b961a2ce36d339092f563c3d75700d0c884c9
chatrdh <crdhuware@gmail.com>
2025-12-14 08:41:54 +0000

    loading the dataset

--- cs336_alignment/baseline.py
+++ cs336_alignment/baseline.py
@@ -1,7 +1,18 @@
-import prompts
 
-from datasets import load_dataset
 
-ds = load_dataset("qwedsacf/competition_math")
+import pandas as pd
+from datasets import Dataset
+
+# Use the same URL as above
+data_url = "https://huggingface.co/datasets/Jiayi-Pan/Countdown-Tasks-3to4/resolve/main/data/train-00000-of-00001.parquet"
+# 1. Load into Pandas
+df = pd.read_parquet(data_url)
+
+# 2. Convert to Hugging Face Dataset
+dataset = Dataset.from_pandas(df)
+
+print("Success! Loaded via Pandas.")
+print(dataset[0])
+
 
 
